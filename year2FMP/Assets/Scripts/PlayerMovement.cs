f90870b09c3b5ed452589c94bf2ed18f97e80ced
buxtonwat3r <500481@chrome.escg.ac.uk>
2021-05-19 15:57:00 +0100

    no message

--- year2FMP/Assets/Scripts/PlayerMovement.cs
+++ year2FMP/Assets/Scripts/PlayerMovement.cs
@@ -6,23 +6,49 @@ public class PlayerMovement : MonoBehaviour
 {
     public CharacterController2D controller;
     public Animator animator;
-     
-    public float runSpeed = 40f;
+
+    public float runSpeed = 0f;
 
     float horizontalMove = 0f;
     bool jump = false;
 
 
 
+    public static bool collectPowerUpSprint = false;
+
+    public static bool collectPowerUpJump = false;
+
+    public static bool collectPowerUpPhase = false;
+
     // Update is called once per frame
     void Update()
     {
+
+        if (collectPowerUpSprint == false)
+        {
+            runSpeed = 100f;
+        }
+
+        else if (collectPowerUpSprint == true)
+        {
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                runSpeed = 150f;
+            }
+            else if (!Input.GetKey(KeyCode.LeftShift))
+            {
+                runSpeed = 100f;
+            }
+
+        }
+
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
         animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
 
-        if (Input.GetButtonDown("Jump"))
 
+
+        if (Input.GetButtonDown("Jump"))
         {
             //FindObjectOfType<AudioManager>().Play("PlayerJump");
             jump = true;
@@ -30,13 +56,33 @@ public class PlayerMovement : MonoBehaviour
             FindObjectOfType<AudioManager>().Play("Jump");
         }
 
+        if (collectPowerUpPhase == false)
+        {
+            runSpeed = 100f;
+        }
+
+        else if (collectPowerUpPhase == true)
+        {
+            if ((Input.GetKey(KeyCode.X)) && ((Input.GetKeyDown(KeyCode.RightArrow)) || (Input.GetKeyDown(KeyCode.D))))
+            {
+                transform.position = new Vector4((transform.position.x + 1f), (transform.position.y), transform.position.z);
+            }
+
+            else if ((Input.GetKey(KeyCode.X)) && ((Input.GetKeyDown(KeyCode.LeftArrow)) || (Input.GetKeyDown(KeyCode.A))))
+            {
+                transform.position = new Vector4((transform.position.x - 1f), (transform.position.y), transform.position.z);
+            }
+            Debug.Log(transform.position.x);
+
+
+        }
+
 
     }
 
-    public void OnLanding ()
+    public void OnLanding()
     {
         animator.SetBool("IsJumping", false);
-
     }
 
     /*private void OnTriggerEnter2D(Collider2D other)
@@ -53,5 +99,20 @@ public class PlayerMovement : MonoBehaviour
         controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
         jump = false;
 
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.CompareTag("PowerUpSprint"))
+        {
+            collectPowerUpSprint = true;
+            collider.gameObject.SetActive(false);
+        }
+
+        if (collider.gameObject.CompareTag("PowerUpPhase"))
+        {
+            collectPowerUpPhase = true;
+            collider.gameObject.SetActive(false);
+        }
     }
 }
